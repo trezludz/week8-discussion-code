7e9c116869efc9c522e39162e20d9065ee356d28
unknown <rgludovico@up.edu.ph>
2023-05-03 15:35:20 +0800

    Sample code

--- lib/providers/todo_provider.dart
+++ lib/providers/todo_provider.dart
@@ -4,52 +4,48 @@
   Description: Sample todo app with Firebase 
 */
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../models/todo_model.dart';
+import '../api/firebase_todo_api.dart';
 
 class TodoListProvider with ChangeNotifier {
-  List<Todo> _todoList = [
-    Todo(
-      completed: true,
-      userId: 1,
-      title: "Grocery",
-    ),
-    Todo(
-      completed: true,
-      userId: 1,
-      title: "Bills",
-    ),
-    Todo(
-      completed: false,
-      userId: 1,
-      title: "Walk dog",
-    ),
-  ];
+  late FirebaseTodoAPI firebaseService;
+  late Stream<QuerySnapshot> _todosStream;
+  
+TodoListProvider() {
+  firebaseService = FirebaseTodoAPI();
+  fetchTodos();
+}
 
-  // getter
-  List<Todo> get todo => _todoList;
+//getter
+Stream<QuerySnapshot> get todos => _todosStream;
 
-  void addTodo(Todo item) {
-    _todoList.add(item);
-    notifyListeners();
-  }
+fetchTodos() {
+  _todosStream = firebaseService.getAllTodos();
+  notifyListeners();
+}
+
+
+void addTodo(Todo item) async {
+  String message = await firebaseService.addTodo(item.toJson(item));
+  print(message);
+  notifyListeners();
+}
 
   void editTodo(int index, String newTitle) {
-    _todoList[index].title = newTitle;
+    //_todoList[index].title = newTitle;
     notifyListeners();
   }
 
-  void deleteTodo(String title) {
-    for (int i = 0; i < _todoList.length; i++) {
-      if (_todoList[i].title == title) {
-        _todoList.remove(_todoList[i]);
-      }
-    }
-    notifyListeners();
-  }
+void deleteTodo(String id) async {
+  String message = await firebaseService.deleteTodo(id);
+  print(message);
+  notifyListeners();
+}
 
   void toggleStatus(int index, bool status) {
-    _todoList[index].completed = status;
+    // _todoList[index].completed = status;
     notifyListeners();
   }
 }
